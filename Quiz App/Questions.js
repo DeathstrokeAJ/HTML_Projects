75a430890b5def98742d03ccf1897497e798c0b6
Adithya J <132728096+DeathstrokeAJ@users.noreply.github.com>
2024-07-03 16:24:01 +0530

    Add files via upload

---
+++ Quiz App/Questions.js
@@ -0,0 +1,246 @@
+const questions = [
+  {
+    number: 1,
+    question: 'What does HTML Stand for?',
+    answer: 'Hyper Text Markup Language',
+    options: [
+      'Home Tool Markup Language',
+      'Hyper Text Markup Language',
+      'Hyperlinks and Text Markup Language',
+      'Hyper Tool Markup Language'
+    ]
+  },
+  {
+    number: 2,
+    question: 'What does CSS stand for?',
+    answer: 'Cascading Style Sheets',
+    options: [
+      'Computer Style Sheets',
+      'Creative Style Sheets',
+      'Colorful Style Sheets',
+      'Cascading Style Sheets'
+    ]
+  },
+  {
+    number: 3,
+    question: 'What does PHP stand for?',
+    answer: 'Hypertext Preprocessor',
+    options: [
+      'Preprocessed Hypertext Page',
+      'Hypertext Preprocessor',
+      'Processor Hypertext Processor',
+      'None of the above'
+    ]
+  },
+  {
+    number: 4,
+    question: 'Which of the below CSS class is used to change the text color of CSS?',
+    answer: 'color',
+    options: [
+      'background-color',
+      'color-background',
+      'color',
+      'None of the above'
+    ]
+  },
+  {
+    number: 5,
+    question: 'Which of the below is the correct syntax to put a line over text in CSS?',
+    answer: 'text-decoration: overline',
+    options: [
+      'text-decoration: line',
+      'text-decoration: none',
+      'text-decoration: overline',
+      'text-decoration: underline'
+    ]
+  },
+  {
+    number: 6,
+    question: 'What does SQL stand for?',
+    answer: 'Structured Query Language',
+    options: [
+      'Structured Query Language',
+      'Strong Question Language',
+      'Simple Query Language',
+      'Stylish Question Language'
+    ]
+  },
+  {
+    number: 7,
+    question: 'Which of the following is used to define a block of code in Python?',
+    answer: 'Indentation',
+    options: [
+      'Brackets',
+      'Indentation',
+      'Parentheses',
+      'Curly braces'
+    ]
+  },
+  {
+    number: 8,
+    question: 'What is the purpose of the "alt" attribute in an <img> tag?',
+    answer: 'Specifies an alternate text for an image',
+    options: [
+      'Specifies the alignment of an image',
+      'Specifies an alternate text for an image',
+      'Specifies the source of an image',
+      'Specifies the size of an image'
+    ]
+  },
+  {
+    number: 9,
+    question: 'Which of the following is the correct way to create a function in JavaScript?',
+    answer: 'function myFunction() {}',
+    options: [
+      'function = myFunction() {}',
+      'function myFunction() {}',
+      'function:myFunction() {}',
+      'function => myFunction() {}'
+    ]
+  },
+  {
+    number: 10,
+    question: 'What is the default value of the "position" property in CSS?',
+    answer: 'static',
+    options: [
+      'absolute',
+      'relative',
+      'fixed',
+      'static'
+    ]
+  },
+  {
+    number: 11,
+    question: 'Which HTML attribute is used to define inline styles?',
+    answer: 'style',
+    options: [
+      'styles',
+      'class',
+      'style',
+      'font'
+    ]
+  },
+  {
+    number: 12,
+    question: 'Which method is used to add an element at the end of an array in JavaScript?',
+    answer: 'push()',
+    options: [
+      'pop()',
+      'shift()',
+      'push()',
+      'unshift()'
+    ]
+  },
+  {
+    number: 13,
+    question: 'Which CSS property is used to control the spacing between elements?',
+    answer: 'margin',
+    options: [
+      'padding',
+      'spacing',
+      'margin',
+      'border'
+    ]
+  },
+  {
+    number: 14,
+    question: 'What does the "href" attribute in an <a> tag specify?',
+    answer: 'The URL of the page the link goes to',
+    options: [
+      'The URL of the page the link goes to',
+      'The text of the link',
+      'The style of the link',
+      'The target of the link'
+    ]
+  },
+  {
+    number: 15,
+    question: 'Which of the following is not a JavaScript data type?',
+    answer: 'Character',
+    options: [
+      'Undefined',
+      'Number',
+      'Boolean',
+      'Character'
+    ]
+  },
+  {
+    number: 16,
+    question: 'Which of the following CSS properties is used to change the background color?',
+    answer: 'background-color',
+    options: [
+      'color',
+      'background-color',
+      'background-image',
+      'background-repeat'
+    ]
+  },
+  {
+    number: 17,
+    question: 'What does the "id" attribute in HTML do?',
+    answer: 'Specifies a unique id for an element',
+    options: [
+      'Specifies a class for an element',
+      'Specifies a unique id for an element',
+      'Specifies the style for an element',
+      'Specifies the source of an element'
+    ]
+  },
+  {
+    number: 18,
+    question: 'Which JavaScript keyword is used to declare a variable?',
+    answer: 'var',
+    options: [
+      'variable',
+      'let',
+      'const',
+      'var'
+    ]
+  },
+  {
+    number: 19,
+    question: 'Which CSS property is used to control the text size?',
+    answer: 'font-size',
+    options: [
+      'text-style',
+      'font-size',
+      'text-size',
+      'font-style'
+    ]
+  },
+ 
+  {
+    number: 20,
+    question: 'What is the correct way to create a comment in CSS?',
+    answer: '/* This is a comment */',
+    options: [
+      '// This is a comment',
+      '<!-- This is a comment -->',
+      '/* This is a comment */',
+      '** This is a comment **'
+    ]
+  },
+  {
+    number: 21,
+    question: 'Which of the following is used to declare a class in JavaScript?',
+    answer: 'class',
+    options: [
+      'className',
+      'class',
+      'object',
+      'constructor'
+    ]
+  },
+ 
+  {
+    number: 22,
+    question: 'Which CSS property is used to make text bold?',
+    answer: 'font-weight',
+    options: [
+      'font-style',
+      'font-weight',
+      'text-transform',
+      'text-weight'
+    ]
+  }
+];
